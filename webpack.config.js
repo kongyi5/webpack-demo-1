b64571503d2e5fb61638d3a32e7951905b26f0e8
kongyi <jerrykongyi1997@gmail.com>
2020-08-30 11:38:16 +0800

    add html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "[name].[contenthash].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ky --github.com",
+      template: "src/assets/index.html",
+    }),
+  ],
 };
