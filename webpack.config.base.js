ce4adea1374882eac9adb5975f7c1ef374ffbf4a
kongyi <jerrykongyi1997@gmail.com>
2020-09-02 21:59:06 +0800

    add less and stylus

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -17,6 +17,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.less$/,
+        loader: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
